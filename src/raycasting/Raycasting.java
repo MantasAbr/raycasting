6d953f45b48cd41b1cd0d8c42ccf0b18b6d54ab2
Mantas Abramavičius <mantasabra@gmail.com>
2020-07-14 12:41:29 +0300

    Wrote the main class. Running the program now just produces a black screen

--- src/raycasting/Raycasting.java
+++ src/raycasting/Raycasting.java
@@ -1,21 +1,113 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package raycasting;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.ArrayList;
+import javax.swing.JFrame;
 
 /**
  *
  * @author Mantas Abramavičius
  */
-public class Raycasting {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Raycasting extends JFrame implements Runnable{
+    private static final long serialVersionUID = 1L;
+    public int mapWidth = 15;
+    public int mapHeight = 15;
+    private Thread thread;
+    private boolean running;
+    private BufferedImage image;
+    public int[] pixels;
+    public static int[][] map =
+        {
+            {1,1,1,1,1,1,1,1,2,2,2,2,2,2,2},
+            {1,0,0,0,0,0,0,0,2,0,0,0,0,0,2},
+            {1,0,3,3,3,3,3,0,0,0,0,0,0,0,2},
+            {1,0,3,0,0,0,3,0,2,0,0,0,0,0,2},
+            {1,0,3,0,0,0,3,0,2,2,2,0,2,2,2},
+            {1,0,3,0,0,0,3,0,2,0,0,0,0,0,2},
+            {1,0,3,3,0,3,3,0,2,0,0,0,0,0,2},
+            {1,0,0,0,0,0,0,0,2,0,0,0,0,0,2},
+            {1,1,1,1,1,1,1,1,4,4,4,0,4,4,4},
+            {1,0,0,0,0,0,1,4,0,0,0,0,0,0,4},
+            {1,0,0,0,0,0,1,4,0,0,0,0,0,0,4},
+            {1,0,0,2,0,0,1,4,0,3,3,3,3,0,4},
+            {1,0,0,0,0,0,1,4,0,3,3,3,3,0,4},
+            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
+            {1,1,1,1,1,1,1,4,4,4,4,4,4,4,4}   
+        };  
+    
+    public Raycasting(){
+        thread = new Thread(this);
+        image = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
+        pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+        setSize(640, 480);
+        setResizable(false);
+        setTitle("veri nice 3d engine yes");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBackground(Color.black);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        start();
     }
     
+    private synchronized void start() {
+        running = true;
+        thread.start();
+    }
+    
+    public synchronized void stop(){
+        running = false;
+        try {
+            thread.join();
+        }
+        catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Buffer strategy is used so that the updates are smoother;
+     * To actually draw the image to the screen, a graphics object is obtained
+     * from the buffer strategy and user to draw our image
+     */
+    public void render() {
+        BufferStrategy bs = getBufferStrategy();
+        if(bs == null){
+            createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+        g.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
+        bs.show();
+    }
+
+    /**
+     * Updates once every 1/60th of a second
+     */
+    @Override
+    public void run() {
+        long lastTime = System.nanoTime();
+        //60 times per second
+        final double ns = 1000000000.0 / 60.0;
+        double delta = 0;
+        requestFocus();
+        while(running){
+            long now = System.nanoTime();
+            delta = delta + ((now - lastTime) / ns);
+            lastTime = now;
+            //Making sure the update is happening only 60 times a second
+            while(delta >= 1){
+                //handles all of the logic restricted time
+                delta--;
+            }
+            //displays to the screen unrestricted time
+            render();
+        }
+    }
+
+    public static void main(String [] args){
+        Raycasting raycasting = new Raycasting();
+    }
 }
