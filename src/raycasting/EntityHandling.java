a49f516a3a3740bfb3f2f704cb8cee7673c3ce3e
Mantas <mantasabra@gmail.com>
2021-09-28 18:28:41 +0300

    Test

--- src/raycasting/EntityHandling.java
+++ src/raycasting/EntityHandling.java
@@ -1,8 +1,11 @@
 package raycasting;
 
+import sprites.Direction;
 import sprites.Entity;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Queue;
 
 public class EntityHandling {
 
@@ -38,6 +41,53 @@ public class EntityHandling {
         for(Entity entity : entities){
             if(entity.getDistance() <= distanceToDealDamage && entity.isHostile())
                 player.setHealthValue(player.getHealthValue() - 0.1);
+        }
+    }
+
+    private Direction findShortestPath(boolean[][] lab, int hx, int hy, int px, int py){
+        Queue<PathfindingNode> queue = new ArrayDeque<>();
+
+        boolean[][] discovered = new boolean[10][10];
+
+        discovered[hx][hy] = true;
+        queue.add(new PathfindingNode(hx, hy, null));
+
+        while (!queue.isEmpty()) {
+            PathfindingNode node = queue.poll();
+
+            // Go breath-first into each direction
+            for (Direction dir : Direction.values()) {
+                int newX = node.x + dir.getDx();
+                int newY = node.y + dir.getDy();
+                Direction newDir = node.initialDir == null ? dir : node.initialDir;
+
+                // Mouse found?
+                if (newX == px && newY == py) {
+                    return newDir;
+                }
+
+                // Is there a path in the direction (= is it a free field in the labyrinth)?
+                // And has that field not yet been discovered?
+                if (!lab[newY][newX] && !discovered[newY][newX]) {
+                    // "Discover" and enqueue that field
+                    discovered[newY][newX] = true;
+                    queue.add(new PathfindingNode(newX, newY, newDir));
+                }
+            }
+        }
+
+        throw new IllegalStateException("No path found");
+    }
+
+    private static class PathfindingNode {
+        final int x;
+        final int y;
+        final Direction initialDir;
+
+        public PathfindingNode(int x, int y, Direction initialDir) {
+            this.x = x;
+            this.y = y;
+            this.initialDir = initialDir;
         }
     }
 }
